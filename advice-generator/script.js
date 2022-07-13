e1aa3ab9575ffdee6dc93bf3a36bb0042b147ddf
danakendybayeva <danka.kent@gmail.com>
2022-07-13 18:26:05 +0600

    advice generator website

---
+++ advice-generator/script.js
@@ -0,0 +1,19 @@
+const adviceID = document.querySelector(".advice__id");
+const adviceText = document.querySelector(".advice__text");
+const dice = document.querySelector(".dice");
+const url = "https://api.adviceslip.com/advice";
+
+window.addEventListener("load", getAdvice);
+dice.addEventListener("click", getAdvice);
+
+function getAdvice() {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.slip);
+      adviceID.innerText = "Advice #" + data.slip.id;
+      adviceText.innerText = data.slip.advice;
+    });
+}
